4f12fa740afaeb003cfd4b5610fcf94fd41e6868
Jovan Ivanovic <jivanovic@renderedtext.com>
2016-07-07 16:09:52 +0200

    Add custom options

--- lib/outside.rb
+++ lib/outside.rb
@@ -5,8 +5,22 @@ module Outside
 
   module_function
 
-  def go(timeout = 5)
+  DEFAULT_TIMEOUT = 5
+
+  def go(*arguments)
+    arguments.is_a?(Hash)
+    timeout = arguments
+      .select { |arg| arg.is_a?(Hash) }
+      .map { |arg| arg.first }
+      .select { |key, value| key == :timeout }
+      .map { |key, value| value }
+      .first || DEFAULT_TIMEOUT
+
+    handle_exceptions = arguments.any? { |arg| arg == :handle_timeout }
+
     Timeout.timeout(timeout) { yield }
+  rescue Timeout::Error => e
+    raise e unless handle_exceptions
   end
 
 end
--- spec/outside_spec.rb
+++ spec/outside_spec.rb
@@ -1,7 +1,41 @@
 require 'spec_helper'
 
 describe Outside do
+
+  subject { Outside }
+
   it 'has a version number' do
     expect(Outside::VERSION).not_to be nil
   end
+
+  describe ".go" do
+    it "wraps the yield in a timeout block" do
+      expect(Timeout).to receive(:timeout).with(5)
+
+      Outside.go
+    end
+
+    context "custom timeout" do
+      it "wraps the yield in a timeout block" do
+        timeout = 10
+
+        expect(Timeout).to receive(:timeout).with(10)
+
+        Outside.go({:timeout => 10})
+      end
+    end
+
+    context "timeout" do
+      it "raises the timeout error" do
+        expect { Outside.go({ :timeout => 0.2 }) { sleep 0.5 } }.to raise_error(Timeout::Error)
+      end
+    end
+
+    context "exception handling enabled" do
+      it "handles the timeout error" do
+        expect { Outside.go(:handle_timeout, { :timeout => 0.2 }) { sleep 0.5 } }.not_to raise_error
+      end
+    end
+  end
+
 end
